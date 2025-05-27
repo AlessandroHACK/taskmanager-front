b48c5c1d470bcfef9e7acc90fd86fcfa63dbb098
AlessandroHACK <alessandro.verdin215@gmail.com>
2025-05-27 17:40:11 -0600

    Fix: fix layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,12 +4,11 @@ import { ToastContainer } from "react-toastify";
 export default function AuthLayout() {
     return (
         <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/bg5.svg')" }}>
-            <div className="py-10 lg:py-20 mx-auto w-[450px] ">
-            <Link to={'/auth/login'}>
-                  <h1 className="text-white font-bold text-5xl text-center" >TaskManager</h1>
-                  {/* <Logo/> */}
-                  </Link>
-                <div>
+            <div className="py-8 lg:py-20 mx-auto w-full max-w-md px-4">
+                <Link to={'/auth/login'}>
+                    <h1 className="text-white font-bold text-4xl md:text-5xl text-center">TaskManager</h1>
+                </Link>
+                <div className="mt-6">
                     <Outlet />
                 </div>
             </div>
